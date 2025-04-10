a8911aa731020562e5f8377ebb673e767ac35e50
Shahriar Swim <shahriarswim01@gmail.com>
2025-04-10 18:51:55 +0600

    feat(settings): add form for customer and store details
    Introduce a form in the settings page to capture customer name, store UUID, username, and password. This allows users to input and save their store-specific details for better customization and functionality.

--- app/routes/app.settings.tsx
+++ app/routes/app.settings.tsx
@@ -1,17 +1,63 @@
-import { Card, Layout, Page, Text, BlockStack } from "@shopify/polaris";
+import {
+  Card,
+  Layout,
+  Page,
+  BlockStack,
+  FormLayout,
+  TextField,
+  Button,
+} from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import { useState } from "react";
 
 export default function SettingsPage() {
+  const [formData, setFormData] = useState({
+    customerName: "",
+    storeUUID: "",
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (value: string, field: keyof typeof formData) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+  };
+
   return (
     <Page>
       <TitleBar title="Settings" />
       <Layout>
         <Layout.Section>
           <Card>
-            <BlockStack gap="300">
-              <Text as="p" variant="bodyMd">
-                Settings Page.
-              </Text>
+            <BlockStack gap="400">
+              <FormLayout>
+                <TextField
+                  label="Customer name (Company name)"
+                  autoComplete="off"
+                  value={formData.customerName}
+                  onChange={(value) => handleChange(value, "customerName")}
+                />
+                <TextField
+                  label="Store UUID"
+                  autoComplete="off"
+                  value={formData.storeUUID}
+                  onChange={(value) => handleChange(value, "storeUUID")}
+                />
+                <TextField
+                  label="Username"
+                  autoComplete="off"
+                  value={formData.username}
+                  onChange={(value) => handleChange(value, "username")}
+                />
+                <TextField
+                  label="Password"
+                  type="password"
+                  autoComplete="off"
+                  value={formData.password}
+                  onChange={(value) => handleChange(value, "password")}
+                />
+              </FormLayout>
+
+              <Button>Save</Button>
             </BlockStack>
           </Card>
         </Layout.Section>
